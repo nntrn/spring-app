c86813b6177ebe371a0780afa034c095ce2b798e
annie tran <nntrn@me.com>
2019-03-28 08:45:58 -0500

    jquery to filter table rows in search bar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -8,4 +8,13 @@
 
 $(document).ready(function () {
     $('.format-phone').mask('(000)-000-0000')
-});
+})
+
+$(document).ready(function(){
+  $("#global-search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("table tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+})
