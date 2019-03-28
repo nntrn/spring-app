8711add2bd3ff17908e88d9164379316dbfcb854
annie tran <nntrn@me.com>
2019-03-28 09:35:54 -0500

    change preventDefault to stopPropagation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,20 +1,19 @@
-!function(e) {
-  "use strict";
-  e(".confirm-delete").on("click", function(event) {
-       event.preventDefault();
-   confirm("are you sure you want to delete?");
-  })
-}(jQuery);
-
 $(document).ready(function () {
-    $('.format-phone').mask('(000)-000-0000')
-})
 
-$(document).ready(function(){
-  $("#global-search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("table tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
+    // pop up dialog when a user attempts to delete 
+    $('.confirm-delete').on("click", function (e) {
+        e.stopPropagation();
+        confirm("are you sure you want to delete?");
+    })
+
+    // format phone number fields
+    $('.format-phone').mask('(000)-000-0000')
+
+    // filter tables using the topbar search input 
+    $("#global-search").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("table tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        })
+    })
 })
