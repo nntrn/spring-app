aed5cb35f6a1d3f97034fb605b56af924203acad
annie tran <nntrn@me.com>
2019-03-29 17:11:14 -0500

    sql appointments table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,3 @@
--- CONNECTION: name=SQLite - spring-app.db
-
 DROP TABLE IF EXISTS clients;
 DROP TABLE IF EXISTS pets;
 DROP TABLE IF EXISTS appointments;
@@ -26,9 +24,9 @@ CREATE INDEX IF NOT EXISTS pets_name_index ON pets (name);
 CREATE TABLE appointments(
 	id integer PRIMARY KEY AUTOINCREMENT,
 	pet_id integer REFERENCES pets(id),
-	appt_type varchar(25),
-	appt_date varchar(25),
-	appt_time varchar(25),
+	appt_type text,
+	appt_date DATE,
+	appt_time integer,
 	created DATE DEFAULT CURRENT_DATE
 );
 
@@ -41,18 +39,38 @@ CREATE TABLE IF NOT EXISTS users (
 	role varchar(255) DEFAULT 'USER'
 );
 
+-- additional tables
 
--- misc tables
-
+DROP TABLE IF EXISTS user_client;
+DROP TABLE IF EXISTS veterinarians;
+DROP TABLE IF EXISTS vet_appointments;
 DROP TABLE IF EXISTS visits;
 
-CREATE TABLE IF NOT EXISTS  visits (
-  id          INTEGER PRIMARY KEY AUTOINCREMENT,
-  pet_id      INTEGER NOT NULL REFERENCES pets(id),
-  visit_date  DATE,
-  created 	  DATE DEFAULT CURRENT_DATE,
-  description VARCHAR(255)
+CREATE TABLE IF NOT EXISTS user_client (
+	client_id integer REFERENCES clients(id),
+	user_id integer REFERENCES users(id) DEFAULT NULL
 );
 
-CREATE INDEX IF NOT EXISTS  visits_pet_id_index ON visits (pet_id);
+CREATE TABLE IF NOT EXISTS veterinarians(
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name text
+);
+
+CREATE INDEX IF NOT EXISTS vet_index ON veterinarians (name);
+
+CREATE TABLE IF NOT EXISTS vet_appointments (
+	vet_id integer REFERENCES veterinarians(id),
+	appt_id integer REFERENCES appointments(id)
+);
+
+CREATE TABLE IF NOT EXISTS visits (
+	id          INTEGER PRIMARY KEY AUTOINCREMENT,
+	pet_id      INTEGER NOT NULL REFERENCES pets(id),
+  	visit_date  DATE,
+  	created 	DATE DEFAULT CURRENT_DATE,
+  	description VARCHAR(255)
+);
+
+CREATE INDEX IF NOT EXISTS visits_pet_id_index ON visits (pet_id);
+
 
