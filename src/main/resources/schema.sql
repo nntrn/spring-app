01e1ae3409812d6c2ecbcc8f56232d9c5c0786b5
annie tran <nntrn@me.com>
2019-04-09 09:08:14 -0500

    sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS appointments;
 DROP TABLE IF EXISTS pets;
 DROP TABLE IF EXISTS clients;
-DROP TABLE IF EXISTS appointments;
+DROP TABLE IF EXISTS types;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS clients(
@@ -11,60 +11,56 @@ CREATE TABLE IF NOT EXISTS clients(
     address TEXT
 );
 
+CREATE TABLE IF NOT EXISTS types (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
 CREATE TABLE IF NOT EXISTS pets(
     id integer PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     gender TEXT,
     altered boolean,
-    client_id integer REFERENCES clients(id) ON DELETE CASCADE
+    client_id integer REFERENCES clients(id) ON DELETE CASCADE,
+    type_id integer REFERENCES types(id)
 );
 
-CREATE INDEX IF NOT EXISTS pets_name_index ON pets(name);
+/*
+close_id: 
+-- 1: open
+-- 2: completed
+-- 3: overdue
+-- 3: noshow
+-- 4. canceled
 
+appt_time
+700|800|900|1000|1100|1200|1300|1400|1500|1600|1700|730|830|930|1030|1130|1230|1330|1430|1530|1630|1730
+*/
 CREATE TABLE appointments(
     id integer PRIMARY KEY AUTOINCREMENT,
     pet_id integer REFERENCES pets(id),
-    appt_type text,
-    appt_date TEXT,
-    appt_time integer,
-    created TEXT DEFAULT CURRENT_DATE
+    reason text,
+    date DATE,
+    time integer,
+    visit_type TEXT,
+    close_type TEXT DEFAULT "Open"
 );
-
-CREATE INDEX IF NOT EXISTS appt_pet_id_index ON appointments(pet_id);
 
 CREATE TABLE IF NOT EXISTS users(
     id integer PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     encoded_password TEXT NOT NULL,
-    role TEXT;
+    role TEXT
 );
 
---additional tables
-
-DROP TABLE IF EXISTS veterinarians;
-DROP TABLE IF EXISTS vet_schedules;
-DROP TABLE IF EXISTS walkins;
-
-CREATE TABLE IF NOT EXISTS veterinarians(
+DROP TABLE IF EXISTS deleted_pets;
+CREATE TABLE IF NOT EXISTS deleted_pets(
     id integer PRIMARY KEY AUTOINCREMENT,
-    name text,
-		day_off integer DEFAULT '7' --clinic is closed on sunday
+    date_deleted TEXT DEFAULT CURRENT_DATE,
+    pet_id integer,
+    name TEXT,
+    gender TEXT,
+    altered boolean,
+    type integer,
+    client_id integer
 );
-
-CREATE INDEX IF NOT EXISTS vet_index ON veterinarians(name);
-
-CREATE TABLE IF NOT EXISTS vet_schedules(
-    vet_id integer REFERENCES veterinarians(id),
-    appt_id integer REFERENCES appointments(id),
-		duration integer
-);
-
-CREATE TABLE IF NOT EXISTS walkins(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    pet_id INTEGER NOT NULL REFERENCES pets(id),
-    visit_date TEXT,
-    created TEXT DEFAULT CURRENT_DATE,
-    description TEXT
-);
-
-CREATE INDEX IF NOT EXISTS visits_pet_id_index ON visits(pet_id);
